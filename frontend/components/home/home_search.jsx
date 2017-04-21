a4ef200e4a15c137776b34f1a230f7c3fd13bc8a
Rithy Huot <RithyBHuot@gmail.com>
2017-04-21 18:27:39 -0400

    Update Searchbar css

--- frontend/components/home/home_search.jsx
+++ frontend/components/home/home_search.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
 const SearchBar = (props) => {
+  const redirectToHome = () => {
+    if (props.location.pathname !== '/home') {
+      props.router.push('/home');
+    }
+  };
+
   return (
     <div className='home-search-container'>
       <div className='home-search'>
         <div className='search'>
-          <input type='search' placeholder='All Groups'/>
-          <span>within 10miles of Location</span>
+          <input type='search' placeholder=' All Groups'/>
+          <span>within 10 miles of New York, NY</span>
         </div>
       </div>
       <div className='home-nav'>
-        <div>
+        <div className='home-groups'>
+          <button onClick={ redirectToHome }>
+            Groups
+          </button>
         </div>
-        <div>
+        <div className='home-calendar'>
+          <button>
+            Calendars
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default SearchBar;
+export default withRouter(SearchBar);
