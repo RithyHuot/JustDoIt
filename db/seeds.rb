fcf6dfe0a220ce87006773c440e3649f88a8051f
Rithy Huot <RithyBHuot@gmail.com>
2017-04-22 01:38:13 -0400

    Modify db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
+Group.destroy_all
 
 #Demo account
 User.create!(email: 'demo@justdoit.com', password: 'password123',
              first_name: 'Jon', last_name: 'Snow')
 
 #seeds users
-
 10.times do
   User.create!(email: Faker::Internet.email, password: 'password123',
                first_name: Faker::Name.first_name,
@@ -23,14 +15,23 @@ end
 users = User.all
 
 #generate random images
-rss = Urss.at("http://www.flickr.com/services/feeds/photos_public.gne?format=rss_200"); true
-images = rss.entries.map { |entry| entry.medias.collect(&:content_url) }.flatten
+# rss = Urss.at("http://www.flickr.com/services/feeds/photos_public.gne?format=rss_200"); true
+# images = rss.entries.map { |entry| entry.medias.collect(&:content_url) }.flatten
 
-10.times do
+#seeds groups
+18.times do
   users.sample.groups.create!(name: Faker::StarWars.vehicle,
                               category: Faker::StarWars.specie,
                               location: Faker::StarWars.planet,
                               description: Faker::StarWars.quote,
                               founded: Faker::Date.backward(100),
-                              image_url: images.sample)
+                              image_url: "/images/group/#{rand(30)}.jpg")
+end
+
+groups = Group.all
+
+50.times do
+  group_id = rand(17)
+  user_id = rand(9)
+  groups[group_id].user_ids = groups[group_id].user_ids.push(users[user_id].id)
 end
