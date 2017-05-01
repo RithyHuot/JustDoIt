24237e50e20ef9fe69ae6b2fb30716cfaadca9cf
Rithy Huot <RithyBHuot@gmail.com>
2017-04-30 20:43:00 -0400

    Fix category css

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -8,75 +8,72 @@ class Explore extends React.Component {
         <div className='explore-text'>
           <span >Explore</span>
         </div>
-        <div className='first-explore'>
-          <div className='first-inner'>
+        <ul className='first-explore'>
+          <li>
             <Link to={`/category/Movements`}>
               <div id='img1'>
                 <img src='/images/movements.png'/>
               </div>
               <span> Movements </span>
             </Link>
-          </div>
-          <div className='second-inner'>
+          </li>
+          <li>
             <Link to={`/category/Outdoors & Adventure`}>
               <div id='img2'>
-              <img src='/images/camp.png'/>
+                <img src='/images/camp.png'/>
               </div>
               <span> Outdoors & Adventure </span>
             </Link>
-          </div>
-          <div className='third-inner'>
+          </li>
+          <li>
             <Link to={`/category/Tech`}>
               <div id='img3'>
-              <img src='/images/tech.png'/>
+                <img src='/images/tech.png'/>
               </div>
               <span> Tech </span>
             </Link>
-          </div>
-          <div className='fourth-inner'>
+          </li>
+          <li>
             <Link to={`/category/Family`}>
               <div id='img4'>
-              <img src='/images/family.png'/>
+                <img src='/images/family.png'/>
               </div>
               <span> Family </span>
             </Link>
-          </div>
-        </div>
-
-        <div className='second-explore'>
-          <div className='second-inner'>
+          </li>
+          <li>
             <Link to={`/category/Health & Wellness`}>
               <div id='img5'>
-              <img src='/images/health.png'/>
+                <img src='/images/health.png'/>
               </div>
               <span> Health & Wellness </span>
             </Link>
-          </div>
-          <div className='second-inner'>
+          </li>
+          <li>
             <Link to={`/category/Sports & Fitness`}>
               <div id='img6'>
-              <img src='/images/sport.png'/>
+                <img src='/images/sport.png'/>
               </div>
               <span> Sports & Fitness </span>
             </Link>
-          </div>
-          <div className='third-inner'>
+          </li>
+          <li>
             <Link to={`/category/Learning`}>
               <div id='img7'>
-              <img src='/images/learning.png'/>
+                <img src='/images/learning.png'/>
               </div>
               <span> Learning </span>
             </Link>
-          </div>
-          <div className='fourth-inner'>
+          </li>
+          <li>
             <Link to={`/category/Photography`}>
               <div id='img8'>
-              <img src='/images/photography.png'/>
+                <img src='/images/photography.png'/>
               </div>
               <span> Photography </span>
             </Link>
-          </div>
-        </div>
+          </li>
+        </ul>
       </div>
     );
   }
